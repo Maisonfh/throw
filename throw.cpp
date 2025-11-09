d43d67ac6dcf7c3dc2ddcbdfd7b690b2ada818b4
Аrtur <barkov.as2020@gmail.com>
2025-11-09 15:41:18 +0200

    gggg

--- throw.cpp
+++ throw.cpp
@@ -1,1 +1,38 @@
-﻿
+﻿#include <iostream>
+#include <thread>
+using namespace std;
+
+int arr[20];
+int index = 0; 
+
+void producer() {
+    for (int i = 1; i <= 20; i++) {
+        arr[index] = i; 
+        index++; 
+        cout << "Виробник Додав: " << i << endl;
+
+  
+        for (volatile int j = 0; j < 10000000; j++);
+    }
+}
+
+void consumer() {
+    int consumed = 0;
+    while (consumed < 20) {
+        if (consumed < index) {
+            cout << "Взяв: " << arr[consumed] << endl;
+            consumed++;
+        }
+    }
+}
+
+int main() {
+	setlocale(LC_ALL, "Ukr");
+    thread t1(producer);
+    thread t2(consumer);
+
+    t1.join();
+    t2.join();
+
+    cout << endl << "Програма завершена" << endl;
+}
